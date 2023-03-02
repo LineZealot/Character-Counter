5154f353f83163b61b2719428ac1d14017163527
LineZealot <holdenmountain@gmail.com>
2023-03-02 09:46:44 -0600

    Displays character count from on page input tag

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function run() {
+    // get input text
+    const inputField = document.querySelector('#charCount-input');
+    // get submit button
+    const inputButton = document.querySelector('.submit');
+    // get output display
     const display = document.querySelector('.charCount-display');
+    // function to get the string and return the character count
     function characterCount() {
-        const input = prompt("Input string:");
+        const input = inputField.value;
         return input.length;
     }
-    display.textContent = characterCount();
+    // update character count on button click
+    inputButton.addEventListener('click', () => {
+        display.textContent = characterCount();
+    });
 }
 run();
